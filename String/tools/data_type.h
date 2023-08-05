758c4ed9772725e306a479daeea9bbc0a17bd47d
RhishikeshA11 <RhishikeshA1@verifone.com>
2023-08-05 15:03:11 +0200

    Start of string implementation

---
+++ String/tools/data_type.h
@@ -0,0 +1,16 @@
+#ifndef DATA_TYPES
+#define DATA_TYPES
+
+typedef unsigned char s_type1;
+typedef unsigned short int s_type2;
+typedef unsigned int s_type4;
+typedef unsigned long int s_type8;
+
+typedef char s8;
+typedef unsigned char u8;
+typedef short int s16;
+typedef unsigned short int u16;
+typedef int s32;
+typedef unsigned int u32;
+
+#endif
