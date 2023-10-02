e59100d52be28c71e87b4c542a2227a3023d63c7
RhishikeshA11 <RhishikeshA1@verifone.com>
2023-10-02 12:18:36 +0200

    3. continue

--- Projects/TextEditor/kilo.c
+++ Projects/TextEditor/kilo.c
@@ -3,6 +3,7 @@
 #include<errno.h>
 #include<ctype.h>
 #include<unistd.h>
+#include<sys/ioctl.h>
 #include<termios.h>
 #include<stdlib.h>
 
@@ -10,18 +11,26 @@
 #define CTRL_KEY(k) ((k) & 0x1F)
 
 /***data***/
-struct termios orig_termios;
+struct editorConfig
+{
+	int screenrows;
+	int screencols;
+	struct termios orig_termios;
+};
+struct editorConfig E;
 
 /***terminal***/
 void die(const char *s)
 {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     perror(s);
     exit(1);
 }
 
 void disableRawMode()
 {
-    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     {
         die("tcsetattr");
     }
@@ -29,11 +38,11 @@ void disableRawMode()
 
 void enableRawMode()
 {
-    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    if(tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
     {
         die("tcgetattr");
     }
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     atexit(disableRawMode);
     
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
@@ -63,10 +72,91 @@ char editorReadKey()
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols)
+{
+	char buf[32];
+	unsigned int i = 0;
+	if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+	{
+		return -1;
+	}
+
+	while(i<sizeof(buf)-1)
+	{
+		if(read(STDIN_FILENO, &buf[i], 1) != 1)
+		{
+			break;
+		}
+		if(buf[i] == 'R')
+		{
+			break;
+		}
+		i++;
+	}
+	buf[i] = '\0';
+	if( (buf[0] != '\x1b') || (buf[1] != '[') )
+	{
+		return -1;
+	}
+	if(sscanf(&buf[2], "%d;%d", rows,cols) != 2)
+	{
+		return -1;
+	}
+
+	return 0;
+}
+
+int getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+
+    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+    	if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+    	{
+    		return -1;
+    	}
+    	return getCursorPosition(rows,cols);
+    }
+    else
+    {
+    	*rows = ws.ws_row;
+    	*cols = ws.ws_col;
+    	return 0;
+    }
+}
+
+/***append buffer***/
+struct abuf
+{
+	char *b;
+	int len;
+};
+
+#define ABUF_INIT {NULL,0}
+
 /***output***/
+void editorDrawRows()
+{
+    int y;
+    for(y=0;y<E.screencols;y++)
+    {
+        write(STDOUT_FILENO,"~", 1);
+
+        if(y < E.screencols-1)
+        {
+        	write(STDOUT_FILENO, "\r\n", 2);
+        }
+    }
+}
+
 void editorRefreshScreen()
 {
-    write(STDOUT_FILENO, "\x1b[2J",4);
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    
+    editorDrawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /***input***/
@@ -77,19 +167,32 @@ void editorProcessKeyPress()
     switch(c)
     {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }
 }
 
 /***init***/
+void initEditor()
+{
+	if(getWindowSize(&E.screenrows, &E.screencols) == -1)
+	{
+		die("getWindowSize");
+	}
+}
+
 int main()
 {
     enableRawMode();
+    initEditor();
 
     while(1)
     {
+    	editorRefreshScreen();
         editorProcessKeyPress();
     }
     return 0;
 }
+
