f8be62b66cba91267c7299d11100dc47ff827b0e
RhishikeshA11 <RhishikeshA1@verifone.com>
2024-03-30 13:59:06 +0100

    launcher intro

---
+++ Projects/Linux/sys/services/sys_startup/include/service_launcher.h
@@ -0,0 +1,18 @@
+#ifndef SERVICE_LAUNCHER
+#define SERVICE_LAUNCHER
+
+namespace service_launcher
+{
+    class ServiceLauncher
+    {
+        ServiceLauncher()
+        {
+
+        }
+
+        private:
+            
+    };
+}
+
+#endif
--- Projects/Linux/sys/services/sys_startup/src/sys_startup.cpp
+++ Projects/Linux/sys/services/sys_startup/src/sys_startup.cpp
@@ -2,13 +2,15 @@
 #include <unistd.h>
 
 #include "abstracted_api.h"
+#include "service_launcher.h"
 
 using namespace std;
 using namespace abstracted_api;
+using namespace service_launcher;
 
 int main(int argc, char **argv)
 {
 	printf("Hello from startup app PID [%d]\n", getpid());
 	// set_working_dir(getSysShareRoot().c_str());
-	
+	ServiceLauncher launcher;
 }
