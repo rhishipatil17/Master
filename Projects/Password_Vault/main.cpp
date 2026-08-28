db868b125a60dc9c893b00fd85a2ffb771211577
rhishipatil17 <rhishipatil17@gmail.com>
2026-08-28 15:04:48 +0530

    Repo cleanup and repo_branches script add

--- Projects/Password_Vault/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "VaultCLI.h"
-
-int main()
-{
-    // We will save the encrypted file in the current directory as "my_passwords.vault"
-    VaultCLI cli("my_passwords.vault");
-    cli.run();
-    return 0;
-}
