a61c1c4375480491f585f30c78a58892125c7795
rhishipatil17 <rhishipatil17@gmail.com>
2026-08-28 18:22:23 +0530

    Submodule cleanup

--- Projects/Database/src/Db_DataMan.cpp
+++
@@ -1,7 +0,0 @@
-#include "Db_DataMan.h"
-
-DbMain::DbDataMan& DbMain::DbDataMan::getInstance()
-{
-    static DbDataMan instance;
-    return instance;
-}
--- Projects/Database/src/Db_IdxMan.cpp
+++
@@ -1,7 +0,0 @@
-#include "Db_IdxMan.h"
-
-DbMain::DbIdxMan& DbMain::DbIdxMan::getInstance()
-{
-    static DbIdxMan instance;
-    return instance;
-}
